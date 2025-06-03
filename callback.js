32d9e6d365d045bb90b6d50dec397a3084b5025d
Sahil Gupta <102417163+SG3659@users.noreply.github.com>
2025-06-03 13:00:41 +0530

    Update callback.js

--- callback.js
+++ callback.js
@@ -1,3 +1,14 @@
+A JavaScript callback is a function which is to be executed
+after another function has finished execution.
+
+Simply said:- Any function that is
+passed as an argument to another
+function so that it can be executed
+in that other function is called as a
+callback function. This results in
+
+callback hell.
+
 function x(y){
     y();
 }
