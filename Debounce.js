4a2518a9fbeca5a34626454ca5d8ca84aa4a207d
Sahil Gupta <102417163+SG3659@users.noreply.github.com>
2025-06-03 15:52:30 +0530

    Create Debounce.js

---
+++ Debounce.js
@@ -0,0 +1,6 @@
+Debouncing is a strategy used to improve the performance of a feature
+by controlling the time at which a function should be executed.
+
+Simple words:- It delays the execution of your code until the user stops
+performing a certain action for a specified amount of time. It is a
+practice used to improve browser performance.
