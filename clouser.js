2598ebec57cd2e3e718910f138a26135807773e9
Sahil Gupta <102417163+SG3659@users.noreply.github.com>
2025-06-03 12:49:30 +0530

    Create clouser.js

---
+++ clouser.js
@@ -0,0 +1,9 @@
+function clouser(name){
+    let greet ="hii"
+    function inner(){
+        return greet + name + "Welcome to hustlers group";
+    }
+  return inner();
+}
+let a= clouser("sahil");
+console.log(a)
